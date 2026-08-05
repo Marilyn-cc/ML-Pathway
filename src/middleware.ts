eb5183466fae42410442976db2929ac7df36c19a
Maryline <merrylynchekwemoi@gmail.com>
2026-08-05 19:48:42 +0300

    feat: add middleware for basic authentication and secure route access

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get('authorization');
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1];
+    const [user, pwd] = atob(authValue).split(':');
+
+    if (user === process.env.AUTH_USER && pwd === process.env.AUTH_PASS) {
+      return NextResponse.next();
+    }
+  }
+
+  return new NextResponse('Auth required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  });
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all paths except static files and Next internals
+     */
+    '/((?!_next/static|_next/image|favicon.ico).*)',
+  ],
+};
